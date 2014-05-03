d52186dbff82a9f01d2941d09a49dce29ee8b0d6
ssut <ssut@ssut.me>
2014-05-04 02:11:33 +0900

    Add AddStorageForm to forms.py

--- app/phantom/forms.py
+++ app/phantom/forms.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+import os
 import config
 
 from app import lib
@@ -44,6 +45,29 @@ class RegisterForm(Form):
 
         if self.password.data != self.password_check.data:
             self.password_check.errors.append('Password check is not match')
+            return False
+
+        return True
+
+class AddStorageForm(Form):
+    name = TextField('Name', validators=[ v.DataRequired(), v.length(max=64) ])
+    path = TextField('Path', validators=[ v.DataRequired(), v.length(max=512) ])
+
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+
+        if len(self.path.data) > 0 and not self.path.data.startswith('/'):
+            self.path.errors.append('Target path must start with "/" (absolute path)')
+            return False
+
+        if not os.path.isdir(self.path.data):
+            self.path.errors.append('Target path is not directory')
+            return False
+
+        if not os.access(self.path.data, os.W_OK):
+            self.path.errors.append('Target path is not writable')
             return False
 
         return True
